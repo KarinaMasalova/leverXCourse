93628da4d2c14c674c910d42e0c7eaf13b7201e6
karina_alexandrovna <masalova.karina@mail.ru>
2020-11-08 15:52:55 +0300

    feat: create object with data for cards

---
+++ task1/src/data.js
@@ -0,0 +1,53 @@
+const cards = [
+    {
+        photo: 'src/assets/img/1.jpg',
+        engName: 'Dzmitry Antonenka',
+        ruName: 'Дмитрий Антоненко',
+        department: 'Web & Mobile',
+        room: 1608,
+    },
+    {
+        photo: 'src/assets/img/2.jpg',
+        engName: 'Aleh Zhukau',
+        ruName: 'Олег Жуков',
+        department: 'Web & Mobile',
+        room: 1608,
+    },
+    {
+        photo: 'src/assets/img/3.jpg',
+        engName: 'Maxim Podolsky',
+        ruName: 'Максим Подольский',
+        department: 'Web & Mobile',
+        room: 1608,
+    },
+    {
+        photo: 'src/assets/img/4.jpg',
+        engName: 'Anna Belova',
+        ruName: 'Анна Белова',
+        department: 'Web & Mobile',
+        room: 1608,
+    },
+    {
+        photo: 'src/assets/img/5.jpg',
+        engName: 'Vitaliy Vlasov',
+        ruName: 'Виталий Власов',
+        department: 'Web & Mobile',
+        room: 1608,
+    },
+    {
+        photo: 'src/assets/img/6.jpg',
+        engName: 'Stepan Smirnov',
+        ruName: 'Степан Смирнов',
+        department: 'Web & Mobile',
+        room: 1608,
+    },
+    {
+        photo: 'src/assets/img/7.jpg',
+        engName: 'Karina Masalova',
+        ruName: 'Карина Масалова',
+        department: 'Web & Mobile',
+        room: 1608,
+    },
+];
+
+export default cards;
