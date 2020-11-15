d1247315f02e0295ea4548c8d54481ea11b14f25
karina_alexandrovna <masalova.karina@mail.ru>
2020-11-15 22:57:52 +0300

    feat: install bcrypt

--- task1/back/server.js
+++ task1/back/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require("fs");
 const cors = require("cors");
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 
 let cards;
 
