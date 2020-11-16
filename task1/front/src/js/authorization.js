18fe647664cfac61c0706c4e1581b10288123f30
karina_alexandrovna <masalova.karina@mail.ru>
2020-11-16 12:21:31 +0300

    refactor: delete console.log

--- task1/front/src/js/authorization.js
+++ task1/front/src/js/authorization.js
@@ -7,8 +7,6 @@ const authorize = () => {
     }
     fetchAuthorization(body)
         .then(data => {
-            console.log(typeof data);
-            console.log(data)
             sessionStorage.setItem('login', data.login);
             sessionStorage.setItem('status', data.status);
         })
