8d3c147a0d5fff7b241ebe8621d6c2fbbb030c8e
karina_alexandrovna <masalova.karina@mail.ru>
2020-11-23 00:42:00 +0300

    feat: add specific abilities for roles

--- task1/front/react-app/src/js/components/header/header-navigation.js
+++ task1/front/react-app/src/js/components/header/header-navigation.js
@@ -4,8 +4,14 @@ import { Link } from 'react-router-dom';
 export default class HeaderNavigation extends Component {
     constructor(props) {
         super(props);
+        this.status = sessionStorage.getItem('status');
     }
     render() {
+        const settingsForAdmin = this.status === 'admin' ? (
+            <li className="navigation__item" id="navigation__settings">
+                <Link to="/settings" className="navigation__link">Settings</Link>
+            </li>
+        ) : '';
         return (
             <nav className="header__navigation">
                 <ul className="navigation">
@@ -15,9 +21,7 @@ export default class HeaderNavigation extends Component {
                     <li className="navigation__item">
                         <a href="#" className="navigation__link">Leave Requests</a>
                     </li>
-                    <li className="navigation__item" id="navigation__settings">
-                        <Link to="/settings" className="navigation__link">Settings</Link>
-                    </li>
+                    {settingsForAdmin}
                 </ul>
             </nav>
         );
--- task1/front/react-app/src/js/components/header/header-profile.js
+++ task1/front/react-app/src/js/components/header/header-profile.js
@@ -4,17 +4,34 @@ import { Link } from 'react-router-dom';
 export default class HeaderProfile extends Component {
     constructor(props) {
         super(props);
+        this.status = sessionStorage.getItem('status');
     }
     render() {
-        return (
-            <div className="user-profile">
-                <Link to="/authorize">
-                    <button type="button" className="button button_colored user-profile__button">Log In</button>
-                </Link>
-                <Link to="/register">
-                    <button type="button" className="button button_colored user-profile__button">Register</button>
-                </Link>
-            </div>
-        );
+        const userProfile = this.status !== null 
+            ? (
+                <div className="user-profile">
+                    <div className="user-profile__notification"> 
+                        <span className="ico notification__ico"></span>
+                    </div>
+                    <div className="user-profile__user user">
+                        <span className="user__photo"></span>
+                        <span className="user__name">karina masalova</span>
+                    </div>
+                    <div className="user-profile__power-off">
+                        <span className="ico power-off__ico"></span>
+                    </div>
+                </div>
+            )
+            : (
+                <div className="user-profile">
+                    <Link to="/authorize">
+                        <button type="button" className="button button_colored user-profile__button">Log In</button>
+                    </Link>
+                    <Link to="/register">
+                        <button type="button" className="button button_colored user-profile__button">Register</button>
+                    </Link>
+                </div>
+            );
+        return userProfile;
     }
 }
