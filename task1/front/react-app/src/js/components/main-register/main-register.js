4f30e32d6e8bfd397a84e3d6d0a7b0b7a478a44f
karina_alexandrovna <masalova.karina@mail.ru>
2020-11-23 22:05:48 +0300

    refactor: replace mainRegister class with function

--- task1/front/react-app/src/js/components/main-register/main-register.js
+++ task1/front/react-app/src/js/components/main-register/main-register.js
@@ -3,31 +3,25 @@ import React, { Component } from "react";
 import RegisterForm from './register-form';
 import { fetchRegistration } from '../../functions/repository';
 
-export default class MainAuth extends Component {
-    constructor(props) {
-        super(props);
-    }
+const register = (login, password, status) => {
+    const body = {
+        login: login,
+        password: password,
+        status: status
+    };
+    fetchRegistration(body)
+        .then(data => console.log(data))
+        .catch(err => console.log(err));
+}
 
-    register(login, password, status) {
-        const body = {
-            login: login,
-            password: password,
-            status: status
-        };
-        fetchRegistration(body)
-            .then(data => console.log(data))
-            .catch(err => console.log(err));
-    }
-    
-    render() {
-        return (
-            <main className="main">
-                <div className="wrapper log-in">
-                    <RegisterForm
-                        onFormSubmitted={(login, password, status) => this.register(login, password, status)}
-                    />
-                </div>
-            </main>
-        );
-    }
+export default function MainRegister() {
+    return (
+        <main className="main">
+            <div className="wrapper log-in">
+                <RegisterForm
+                    onFormSubmitted={(login, password, status) => register(login, password, status)}
+                />
+            </div>
+        </main>
+    );
 }
--- task1/front/react-app/src/js/components/main-register/register-form.js
+++ task1/front/react-app/src/js/components/main-register/register-form.js
@@ -1,44 +1,38 @@
-import React, { Component } from 'react'
+import React, { useRef } from 'react'
 
 import Login from '../main-auth/login';
 import Password from '../main-auth/password';
 import RoleSelection from '../main-register/role-selection';
 
-export default class RegisterForm extends Component {
-    constructor(props) {
-        super(props);
-        this.loginInputValue = React.createRef();
-        this.passwordInputValue = React.createRef();
-        this.statusInputValue = React.createRef();
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+export default function RegisterForm(props) {
+    let loginInputValue = useRef(null);
+    let passwordInputValue = useRef(null);
+    let statusInputValue = useRef(null);
 
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault();
         let radioButtons = [
-            this.statusInputValue.current,
-            this.statusInputValue.current.nextElementSibling
+            statusInputValue.current,
+            statusInputValue.current.nextElementSibling
         ];
         radioButtons.forEach((input) => {
             if(input.checked) {
-                this.statusInputValue.current.value = input.value;
+                statusInputValue.current.value = input.value;
             }
         });
-        this.props.onFormSubmitted(
-            this.loginInputValue.current.value,
-            this.passwordInputValue.current.value,
-            this.statusInputValue.current.value
+        props.onFormSubmitted(
+            loginInputValue.current.value,
+            passwordInputValue.current.value,
+            statusInputValue.current.value
         );
     }
-
-    render() {
-        return (
-            <form className="registration__form" method="POST" onSubmit={this.handleSubmit}>
-                <Login refLoginInputValue={this.loginInputValue}/>
-                <Password refPasswordInputValue={this.passwordInputValue}/>
-                <RoleSelection refStatusInputValue={this.statusInputValue}/>
-                <button className="button button_colored form__button" type="submit">Register</button>
-            </form>
-        );
-    }
+    
+    return (
+        <form className="registration__form" method="POST" onSubmit={handleSubmit}>
+            <Login refLoginInputValue={loginInputValue}/>
+            <Password refPasswordInputValue={passwordInputValue}/>
+            <RoleSelection refStatusInputValue={statusInputValue}/>
+            <button className="button button_colored form__button" type="submit">Register</button>
+        </form>
+    );
 }
