4cf474df25aa75385ff843927d5ab5bbab9c044d
karina_alexandrovna <masalova.karina@mail.ru>
2020-12-01 17:10:39 +0300

    feat: install cors

--- final-task/back/server.js
+++ final-task/back/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require("fs");
 const bodyParser = require('body-parser');
+const cors = require("cors");
 
 let requests;
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors());
 
 fs.readFile(__dirname + "/requests.json", (error, data) => {
     if(error) {
