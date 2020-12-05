cf14b212551395afc436e9894f791ff326806e34
karina_alexandrovna <masalova.karina@mail.ru>
2020-12-05 20:18:15 +0300

    feat: make request details popup as react portal

--- final-task/front/src/js/components/main/pop-up/request-details/request-details.js
+++ final-task/front/src/js/components/main/pop-up/request-details/request-details.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from "react-redux";
+import { createPortal } from 'react-dom';
 
 import OneRequestCard from '../../all-requests/one-request-card';
 import FormButton from '../../new-request/new-request-form/form-button';
@@ -19,73 +20,77 @@ export default function RequestDetailsPopup() {
             .catch(err => console.log(err));
     }, [currentCardId]);
 
-    return (
-        <div className="popup-container">
-            <div className="request-details-info">
-                <p className="request-details__title">Request for vacation
+    return createPortal(
+        <>
+            <div className="overlay"></div>
+            <div className="popup-container">
+                <div className="request-details-info">
+                    <p className="request-details__title">Request for vacation
                     <span className="ico copy-icon"></span>
-                </p>
-                <OneRequestCard
-                    className={(currentCardId.type === 'Vacation')
-                        ? 'request-card__icon request-card__icon_vacation'
-                        : (currentCardId.type === 'Sick leave')
-                            ? 'request-card__icon request-card__icon_sick-leave'
-                            : 'request-card__icon request-card__icon_own-expense'}
-                    type={currentCardId.type}
-                    startDate={currentCardId.startDate}
-                    endDate={currentCardId.endDate}
-                    durationInDays={currentCardId.durationInDays}
-                    creationDate={currentCardId.creationDate}
-                    approve={currentCardId.approve}
-                    key={currentCardId.id}
-                />
-                <div className="all-approvers">
-                    <div className="approvers-container">
-                        <ApproversContainerTitle title={'Already approved'}/>
-                        <Approver
-                            className={'people-approvers__icon people-approvers__icon_approved'}
-                            name={'John Smith'}
-                            comments={'Comments: Have a nice vacation!'}
-                        />
-                        <Approver
-                            title={'Already approved'}
-                            className={'people-approvers__icon people-approvers__icon_approved'}
-                            name={'John Smith'}
-                            comments={'Comments: Have a nice vacation!'}
-                        />
+                    </p>
+                    <OneRequestCard
+                        className={(currentCardId.type === 'Vacation')
+                            ? 'request-card__icon request-card__icon_vacation'
+                            : (currentCardId.type === 'Sick leave')
+                                ? 'request-card__icon request-card__icon_sick-leave'
+                                : 'request-card__icon request-card__icon_own-expense'}
+                        type={currentCardId.type}
+                        startDate={currentCardId.startDate}
+                        endDate={currentCardId.endDate}
+                        durationInDays={currentCardId.durationInDays}
+                        creationDate={currentCardId.creationDate}
+                        approve={currentCardId.approve}
+                        key={currentCardId.id}
+                    />
+                    <div className="all-approvers">
+                        <div className="approvers-container">
+                            <ApproversContainerTitle title={'Already approved'} />
+                            <Approver
+                                className={'people-approvers__icon people-approvers__icon_approved'}
+                                name={'John Smith'}
+                                comments={'Comments: Have a nice vacation!'}
+                            />
+                            <Approver
+                                title={'Already approved'}
+                                className={'people-approvers__icon people-approvers__icon_approved'}
+                                name={'John Smith'}
+                                comments={'Comments: Have a nice vacation!'}
+                            />
+                        </div>
+                        <div className="approvers-container">
+                            <ApproversContainerTitle title={'Current approver(s)'} />
+                            <Approver
+                                className={'people-approvers__icon people-approvers__icon_will-mcconnel'}
+                                name={'Will McConnel'}
+                            />
+                        </div>
+                        <div className="approvers-container">
+                            <ApproversContainerTitle title={'Next approver(s)'} />
+                            <Approver
+                                className={'people-approvers__icon people-approvers__icon_john-smith'}
+                                name={'John Smith'}
+                            />
+                            <Approver
+                                className={'people-approvers__icon people-approvers__icon_mike-smith'}
+                                name={'Mike Smith'}
+                            />
+                        </div>
+                        <div className="approvers-container">
+                            <ApproversContainerTitle title={'Documents registration (final step)'} />
+                            <Approver
+                                className={'people-approvers__icon people-approvers__icon_katrin-brown'}
+                                name={'Katrin Brown'}
+                            />
+                        </div>
                     </div>
-                    <div className="approvers-container">
-                        <ApproversContainerTitle title={'Current approver(s)'}/>
-                        <Approver
-                            className={'people-approvers__icon people-approvers__icon_will-mcconnel'}
-                            name={'Will McConnel'}
-                        />
+                    <div className="request-details__buttons">
+                        <FormButton text={'cancel request'} className={'button button_uncolored'} />
+                        <FormButton text={'change'} className={'button button_uncolored'} />
+                        <FormButton text={'submit'} className={'button button_colored'} />
                     </div>
-                    <div className="approvers-container">
-                        <ApproversContainerTitle title={'Next approver(s)'}/>
-                        <Approver
-                            className={'people-approvers__icon people-approvers__icon_john-smith'}
-                            name={'John Smith'}
-                        />
-                        <Approver
-                            className={'people-approvers__icon people-approvers__icon_mike-smith'}
-                            name={'Mike Smith'}
-                        />
-                    </div>
-                    <div className="approvers-container">
-                        <ApproversContainerTitle title={'Documents registration (final step)'}/>
-                        <Approver
-                            className={'people-approvers__icon people-approvers__icon_katrin-brown'}
-                            name={'Katrin Brown'}
-                        />
-                    </div>
-                </div>
-                <div className="request-details__buttons">
-                    <FormButton text={'cancel request'} className={'button button_uncolored'}/>
-                    <FormButton text={'change'} className={'button button_uncolored'}/>
-                    <FormButton text={'submit'} className={'button button_colored'}/>
                 </div>
             </div>
-        </div>
+        </>,
+        document.getElementById("portalDetailsPopup")
     );
 }
